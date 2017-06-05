0d0434f7c97a28c85a0d724db47ae227510b9398
Zsolt Nagy <nagyza@gmail.com>
2017-06-05 21:06:41 +0200

    Change setters and getters and date field

--- src/main/java/com/greenfox/caloriecounter/model/Meal.java
+++ src/main/java/com/greenfox/caloriecounter/model/Meal.java
@@ -1,16 +1,22 @@
 package com.greenfox.caloriecounter.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "meals")
 public class Meal {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
-  private Date date;
+  private String date;
   private String type;
   private String description;
   private int calorie;
@@ -18,42 +24,18 @@ public class Meal {
   public Meal() {
   }
 
+  public Meal(String  date, String type, String description, int calorie) {
+    this.date = date;
+    this.type = type;
+    this.description = description;
+    this.calorie = calorie;
+  }
+
   public Meal(String type, String description, int calorie) {
-    this.date = new Date();
+    Date myDate = new Date();
+    this.date = new SimpleDateFormat("yyyy-MM-dd").format(myDate);
     this.type = type;
     this.description = description;
     this.calorie = calorie;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public void setCalorie(int calorie) {
-    this.calorie = calorie;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public Date getDate() {
-    return date;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public int getCalorie() {
-    return calorie;
   }
 }
